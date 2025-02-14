3b5da7b4404807b998aea8087a1bcdb1070c1df2
Frédéric COLIN <frederic.colin@equadex.net>
2025-02-14 14:43:04 +0100

    DB03 bis

---
+++ Services/DB03RecipesServices.cs
@@ -0,0 +1,28 @@
+﻿using DataContracts;
+using Services.Core;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DB03RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            var cs = "appsettings:AzureRecipesConnectionString".GetConfigValue();
+
+            return GetInternalAll(cs, $"SELECT * FROM Recipes", System.Data.CommandType.Text);
+        }
+
+        public override List<Recipe> GetByTitle(string title)
+        {
+            var cs = "appsettings:AzureRecipesConnectionString".GetConfigValue();
+
+            return GetInternalAll(cs, $"SELECT * FROM Recipes WHERE Title LIKE '%{title}%'", System.Data.CommandType.Text);
+        }
+    }
+}
