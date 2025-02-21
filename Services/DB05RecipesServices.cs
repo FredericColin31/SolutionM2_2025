afc9cab6dcd73529f495c2b76392abd2d93e9b4a
Frédéric COLIN <frederic.colin@equadex.net>
2025-02-21 13:51:05 +0100

    DB05

---
+++ Services/DB05RecipesServices.cs
@@ -0,0 +1,31 @@
+﻿using DataContracts;
+using Microsoft.EntityFrameworkCore.Storage;
+using Services.DataAccessLayer;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DB05RecipesServices : AbstractRecipesServices
+    {
+        public override List<DataContracts.Recipe> GetAll()
+        {
+            using (var context = new BRecipesContext())
+            {
+                return context.Recipes.Select(@r => new DataContracts.Recipe() { Id = @r.Id, Title = @r.Title }).ToList();
+            }
+        }
+
+        public override List<DataContracts.Recipe> GetByTitle(string title)
+        {
+            using (var context = new BRecipesContext())
+            {
+                return context.Recipes.Where(@r => @r.Title.Contains(title)).Select(@r => new DataContracts.Recipe() { Id = @r.Id, Title = @r.Title }).ToList();
+            }
+        }
+    }
+}
